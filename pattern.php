a865aecdb6096ac0bc2f6f4925f608533b5fedcb
Puggan <puggan2puggan.se>
2016-12-28 22:42:15 +0100

    posible to run with no user-input

--- pattern.php
+++ pattern.php
@@ -8,11 +8,26 @@
 		if($argv[1] == '-')
 		{
 			$cli->pattern = pattern::load("php://stdin");
-
 		}
 		else
 		{
 			$cli->pattern = pattern::load($argv[1]);
 		}
 	}
-	$cli->run();
+	if(!empty($argv[2]))
+	{
+		while($cli->pattern->sync())
+		{
+			usleep(100000);
+			$cli->print_pattern();
+		}
+		$cli->pattern->save($argv[2]);
+		if(!empty($argv[3]))
+		{
+			$cli->pattern->export_png($argv[3]);
+		}
+	}
+	else
+	{
+		$cli->run();
+	}
